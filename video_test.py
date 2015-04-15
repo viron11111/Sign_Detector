0931ed15b6513c1779302b5ef764698935acb054
viron11111 <andygrayia@gmail.com>
2015-04-15 12:50:15 -0400

    started adding functions to recognize the shapes.

--- video_test.py
+++ video_test.py
@@ -5,6 +5,111 @@ import cv2
 import math
 import cv2.cv as cv
 
+def find_shape(orig_img, blank_img, p1, p2, nr, mr, dst_frm_cnt):
+    imgray = cv2.cvtColor(orig_img,cv2.COLOR_BGR2GRAY)
+
+    kernel = np.ones((7,7),np.float32)/25
+    imgray_triangle = cv2.filter2D(imgray,-1,kernel)
+
+    width = imgray.shape[0]/2
+    length = imgray.shape[1]/2   
+
+    ret,thresh = cv2.threshold(imgray,230,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    biggest_area = 0
+    biggest_cnt = [0]
+    biggest_M = [0]
+    biggest_approx = [0]
+    cxmax = 0
+    cymax = 0
+
+    for cnt in contours:
+        approx = cv2.approxPolyDP(cnt,0.1*cv2.arcLength(cnt,True),True)
+
+        M = cv2.moments(approx)
+
+        if M['m00'] != 0.0:
+            area = cv2.contourArea(approx)	
+		
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+
+            d = math.sqrt((length-cx)*(length-cx) + (width-cy)*(width-cy))
+
+            if area >= biggest_area and d < distance_from_center:
+                biggest_area = area
+                biggest_cnt[0] = cnt
+                cxmax = cx
+                cymax = cy
+        if biggest_area != 0:
+
+            cv2.drawContours(blank_img,[biggest_cnt[0]],0,(0,0,255),1)
+
+            blank_gray = cv2.cvtColor(blank_img,cv2.COLOR_BGR2GRAY)
+
+            circles = cv2.HoughCircles(blank_gray,cv.CV_HOUGH_GRADIENT,1, width, param1=p1,param2=p2,minRadius=nr,maxRadius=mr)          
+
+            dst = cv2.goodFeaturesToTrack(blank_gray,25,0.15,10)
+            dst = np.int0(dst)
+
+            for i in dst:
+                x,y = i.ravel()
+                cv2.circle(blank_gray,(x,y),4,0,-1)
+            
+            if circles != None:
+                symbol_type = 'circle'
+            else:
+                if len(dst) == 12:
+                    symbol_type = 'cruciform'
+                else:
+                    ret,thresh = cv2.threshold(imgray_triangle,230,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+                    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+                    biggest_area = 0
+                    biggest_cnt = [0]
+                    biggest_M = [0]
+                    biggest_approx = [0]
+                    cxmax = 0
+                    cymax = 0
+
+                    for cnt in contours:
+                        approx = cv2.approxPolyDP(cnt,0.1*cv2.arcLength(cnt,True),True)
+
+                        M = cv2.moments(approx)
+
+                        if M['m00'] != 0.0:
+                            area = cv2.contourArea(approx)	
+		
+                            cx = int(M['m10']/M['m00'])
+                            cy = int(M['m01']/M['m00'])
+
+                            d = math.sqrt((length-cx)*(length-cx) + (width-cy)*(width-cy))
+
+                            if area >= biggest_area and d < distance_from_center:
+                                biggest_area = area
+                                biggest_cnt[0] = cnt
+                                cxmax = cx
+                                cymax = cy
+                        if biggest_area != 0:
+
+                            blank_gray = cv2.cvtColor(blank_img,cv2.COLOR_BGR2GRAY)
+
+                            dst = cv2.goodFeaturesToTrack(blank_gray,25,0.15,10)
+                            dst = np.int0(dst)
+
+                            for i in dst:
+                                x,y = i.ravel()
+
+                            if len(dst) == 3:
+                                symbol_type = 'triangle'
+                            else:
+                                symbol_type = 'circle'  
+    return (symbol_type)
+
+
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 #Hough_circle values
@@ -28,6 +133,7 @@ cnt_hold = [0,0,0]
 
 vid = 'Dock_Simulation_30_degrees.avi'
 #vid = '/home/andy/dock_simulation_vertical.avi'
+
 cap = cv2.VideoCapture(vid)
 
 while(cap.isOpened()):
@@ -36,6 +142,7 @@ while(cap.isOpened()):
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     imgray = cv2.medianBlur(gray,9)
+    #cv2.imshow('distort', imgray)
     ret,thresh = cv2.threshold(imgray,240,255,cv2.THRESH_TOZERO)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     
@@ -46,11 +153,11 @@ while(cap.isOpened()):
     while i < len(contours):
         
         cnt = contours[i]
-	    #print cnt
+        #print cnt
 
         M = cv2.moments(cnt)
         area = cv2.contourArea(cnt)
-	
+
         if area >= sign[0]:
             sign[2] = sign[1]
             cnt_hold[2] = cnt_hold[1]
@@ -66,7 +173,7 @@ while(cap.isOpened()):
         elif area >= sign[2]:
             sign[2] = area
             cnt_hold[2] = cnt
-		
+	
         i += 1
 
     i = 0
@@ -88,75 +195,67 @@ while(cap.isOpened()):
     blank_one[:,0:corner[2]] = (255,255,255)
         
     if (corner[0] < corner[4] and corner[0] < corner [8]):
-        #blank_one is left        
-        one_pos = 0
-
         one_a = frame[corner[1]:corner[1]+corner[3], corner[0]:corner[0]+corner[2]]
         blank_one = np.zeros((corner[3],corner[2],3), np.uint8)
-        blank_one[:,0:corner[2]] = (255,255,255)        
+        blank_one[:,0:corner[2]] = (255,255,255)  
+        sign1 =  find_shape(one_a, blank_one, p1, p2, nr, mr, distance_from_center)      
         cv2.putText(frame_real, "Sign1" , (corner[0], corner[1] - 5), font, .75,(255,0,255),2, cv2.CV_AA)
     elif (corner[0] > corner[4] and corner[0] > corner[8]):
-        #blank_one is right        
-        one_pos = 2
-
         three_a = frame[corner[1]:corner[1]+corner[3], corner[0]:corner[0]+corner[2]]
         blank_three = np.zeros((corner[3],corner[2],3), np.uint8)
-        blank_three[:,0:corner[2]] = (255,255,255)        
+        blank_three[:,0:corner[2]] = (255,255,255)
+        sign3 =  find_shape(three_a, blank_three, p1, p2, nr, mr, distance_from_center)                 
         cv2.putText(frame_real, "Sign3" , (corner[0], corner[1] - 5), font, .75,(255,0,255),2, cv2.CV_AA)
     else:
-        #blank_one is center       
-        one_pos = 1
-
         two_a = frame[corner[1]:corner[1]+corner[3], corner[0]:corner[0]+corner[2]]
         blank_two = np.zeros((corner[3],corner[2],3), np.uint8)
-        blank_two[:,0:corner[2]] = (255,255,255)        
+        blank_two[:,0:corner[2]] = (255,255,255) 
+        sign2 =  find_shape(two_a, blank_two, p1, p2, nr, mr, distance_from_center)
         cv2.putText(frame_real, "Sign2" , (corner[0], corner[1] - 5), font, .75,(255,0,255),2, cv2.CV_AA)
         
 
     if (corner[4] < corner[0] and corner[4] < corner [8]):
-        #blank_one is left        
-        one_pos = 0
-
         one_a = frame[corner[5]:corner[5]+corner[7], corner[4]:corner[4]+corner[6]]
         blank_one = np.zeros((corner[7],corner[6],3), np.uint8)
         blank_one[:,0:corner[6]] = (255,255,255)
+        sign1 =  find_shape(one_a, blank_one, p1, p2, nr, mr, distance_from_center)  
         cv2.putText(frame_real, "Sign1" , (corner[4], corner[5] - 5), font, .75,(255,0,255),2, cv2.CV_AA)
     elif (corner[4] > corner[0] and corner[4] > corner[8]):
         three_a = frame[corner[5]:corner[5]+corner[7], corner[4]:corner[4]+corner[6]]
         blank_three = np.zeros((corner[7],corner[6],3), np.uint8)
-        blank_three[:,0:corner[6]] = (255,255,255)        
+        blank_three[:,0:corner[6]] = (255,255,255)
+        sign3 =  find_shape(three_a, blank_three, p1, p2, nr, mr, distance_from_center)         
         cv2.putText(frame_real, "Sign3" , (corner[4], corner[5] - 5), font, .75,(255,0,255),2, cv2.CV_AA)
     else:
         two_a = frame[corner[5]:corner[5]+corner[7], corner[4]:corner[4]+corner[6]]
         blank_two = np.zeros((corner[7],corner[6],3), np.uint8)
-        blank_two[:,0:corner[6]] = (255,255,255)        
+        blank_two[:,0:corner[6]] = (255,255,255)    
+        sign2 =  find_shape(two_a, blank_two, p1, p2, nr, mr, distance_from_center)    
         cv2.putText(frame_real, "Sign2" , (corner[4], corner[5] - 5), font, .75,(255,0,255),2, cv2.CV_AA)
 
     if (corner[8] < corner[4] and corner[8] < corner [0]):
         one_a = frame[corner[9]:corner[9]+corner[11], corner[8]:corner[8]+corner[10]]
         blank_one = np.zeros((corner[11],corner[10],3), np.uint8)
         blank_one[:,0:corner[10]] = (255,255,255)
+        sign1 =  find_shape(one_a, blank_one, p1, p2, nr, mr, distance_from_center)  
         cv2.putText(frame_real, "Sign1" , (corner[8], corner[9] - 5), font, .75,(255,0,255),2, cv2.CV_AA)
     elif (corner[8] > corner[0] and corner[8] > corner[4]):
         three_a = frame[corner[9]:corner[9]+corner[11], corner[8]:corner[8]+corner[10]]
         blank_three = np.zeros((corner[11],corner[10],3), np.uint8)
         blank_three[:,0:corner[10]] = (255,255,255)
+        sign3 =  find_shape(three_a, blank_three, p1, p2, nr, mr, distance_from_center) 
         cv2.putText(frame_real, "Sign3" , (corner[8], corner[9] - 5), font, .75,(255,0,255),2, cv2.CV_AA)
     else:
         two_a = frame[corner[9]:corner[9]+corner[11], corner[8]:corner[8]+corner[10]]
         blank_two = np.zeros((corner[11],corner[10],3), np.uint8)
         blank_two[:,0:corner[10]] = (255,255,255)
+        sign2 =  find_shape(two_a, blank_two, p1, p2, nr, mr, distance_from_center)
         cv2.putText(frame_real, "Sign2" , (corner[8], corner[9] - 5), font, .75,(255,0,255),2, cv2.CV_AA)
 
-    #one = frame[corner[1]:corner[1]+corner[3], corner[0]:corner[0]+corner[2]]
-    #two = frame[corner[5]:corner[5]+corner[7], corner[4]:corner[4]+corner[6]]
-    #three = frame[corner[9]:corner[9]+corner[11], corner[8]:corner[8]+corner[10]]
-    #two_a = frame[corner[5]:corner[5]+corner[7], corner[4]:corner[4]+corner[6]]
-    #three_a = frame[corner[9]:corner[9]+corner[11], corner[8]:corner[8]+corner[10]]
-
-    #two_a = frame[corner[5]:corner[5]+corner[7], corner[4]:corner[4]+corner[6]]
-    #blank_two = np.zeros((corner[7],corner[6],3), np.uint8)
-    #blank_two[:,0:corner[6]] = (255,255,255)
+    print "sign1: " + sign1
+    print "sign2: " + sign2
+    print "sign3: " + sign3
+    
 
 
 
@@ -177,6 +276,7 @@ while(cap.isOpened()):
 
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+    #need to move this
     cv2.circle(frame_real,(length + corner[0],width + corner[1]), 2, (0,255,255),-1)
 
     biggest_area = 0
@@ -193,7 +293,7 @@ while(cap.isOpened()):
 
         if M['m00'] != 0.0:
             area = cv2.contourArea(approx)	
-			
+		
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
 
@@ -250,7 +350,7 @@ while(cap.isOpened()):
 
                         if M['m00'] != 0.0:
                             area = cv2.contourArea(approx)	
-			
+		
                             cx = int(M['m10']/M['m00'])
                             cy = int(M['m01']/M['m00'])
 
@@ -324,7 +424,7 @@ while(cap.isOpened()):
 
         if M['m00'] != 0.0:
             area = cv2.contourArea(approx)	
-			
+		
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
 
@@ -339,7 +439,7 @@ while(cap.isOpened()):
         if biggest_area != 0:
             #print len(biggest_cnt[0])
 
-            cv2.drawContours(blank_two,[biggest_cnt[0]],0,(0,0,255),1)
+            
 
             blank_gray2 = cv2.cvtColor(blank_two,cv2.COLOR_BGR2GRAY)
 
@@ -394,7 +494,7 @@ while(cap.isOpened()):
 
         if M['m00'] != 0.0:
             area = cv2.contourArea(approx)	
-			
+		
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
 
@@ -425,7 +525,7 @@ while(cap.isOpened()):
 
             cv2.circle(frame_real,(cxmax + corner[8],cymax + corner[9]), 1, (0,255,0),-1)
 
-            print len(dst)
+            #print len(dst)
 
             if circles != None:
                 cv2.putText(blank_gray3, "circle", (cxmax+10, cymax + 25), font, .5,(0,0,255),1, cv2.CV_AA)
@@ -438,17 +538,14 @@ while(cap.isOpened()):
                     cv2.putText(blank_gray3, "circle", (cxmax+10, cymax + 25), font, .5,(0,0,255),1, cv2.CV_AA) 
 
 #***************************************************************************************** Display
-	    
+        
     cv2.imshow('actual', frame_real)
-    cv2.imshow('two', blank_gray2)
-    cv2.imshow('one', blank_gray1)
-    cv2.imshow('three', blank_gray3)
-    #cv2.imwrite ("ellipse.jpg", blank_gray1)
-    #cv2.imwrite ("cruciform.jpg", blank_gray2)
-    #cv2.imwrite ("triangle.jpg", blank_gray3)
-    cv2.moveWindow('one', 20, 20)
-    cv2.moveWindow('two', 20, 180)
-    cv2.moveWindow('three', 20, 340)
+#    cv2.imshow('two', blank_gray2)
+#    cv2.imshow('one', blank_gray1)
+#    cv2.imshow('three', blank_gray3)
+#    cv2.moveWindow('one', 20, 20)
+#    cv2.moveWindow('two', 20, 180)
+#    cv2.moveWindow('three', 20, 340)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
